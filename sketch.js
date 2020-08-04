a0c268ec0accab38d6809d7b487b5c70e8fb6464
deepali-m <deepalimokashi@gmail.com>
2020-08-04 18:27:18 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-
+var platform;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -17,9 +17,9 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform = new Ground(150,305,300,170);
     box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    box2 = new Box(920,320,70,70);''
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
@@ -59,4 +59,10 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
+
+
+//1 working directory
+//2 staging area
+//3 repository   local / remote
